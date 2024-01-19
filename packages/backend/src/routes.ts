b56f43b5fb0fc437bd56f6a29c7ae6ac2c58af48
RicardoKd <richardo.kadyan@gmail.com>
2024-01-19 22:23:16 +0200

    feat: server structure refactored

---
+++ packages/backend/src/routes.ts
@@ -0,0 +1,39 @@
+import { Response } from 'express';
+
+import server from './server';
+import connectDB from './db';
+import { ILoginRequest, IQueryRequest } from './types';
+
+server.post('/login', async ({ body }: ILoginRequest, res: Response) => {
+  try {
+    const client = await connectDB(body.user, body.password);
+
+    let result = await client.query('select get_my_role()');
+
+    const role = result.rows[0].get_my_role;
+
+    client.end();
+
+    return res.json({ role: role });
+  } catch (error: any) {
+    console.error(error.message);
+
+    return res.status(401).json({ error: error.message });
+  }
+});
+
+server.post('/query', async ({ body }: IQueryRequest, res: Response) => {
+  try {
+    const client = await connectDB(body.user, body.password);
+
+    const result = await client.query(body.query, body.variables);
+
+    client.end();
+
+    return res.json(result);
+  } catch (error: any) {
+    console.error(error.message);
+
+    return res.status(500).json({ error: error.message });
+  }
+});
--- packages/backend/src/server.ts
+++ packages/backend/src/server.ts
@@ -1,54 +1,16 @@
 /* eslint-disable no-console */
 import cors from 'cors';
+import express from 'express';
 import bodyParser from 'body-parser';
-import express, { Response } from 'express';
 
-import connectDB from './db';
-import { PORT, PG_CONF, ROLES } from './const';
-import { ILoginRequest, IQueryRequest } from './types';
+import { PORT } from './const';
 
-const app = express();
+const server = express();
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use(cors());
+server.use(bodyParser.urlencoded({ extended: false }));
+server.use(bodyParser.json());
+server.use(cors());
 
-app.post('/login', async ({ body }: ILoginRequest, res: Response) => {
-  try {
-    const client = await connectDB(body.user, body.password);
-
-    let result = await client.query('select get_my_role()');
-
-    const role = result.rows[0].get_my_role;
-
-    client.end();
-
-    return res.json({ role: role });
-  } catch (error: any) {
-    console.error(error.message);
-
-    return res.status(401).json({ error: error.message });
-  }
-});
-
-app.post('/query', async ({ body }: IQueryRequest, res: Response) => {
-  try {
-    const client = await connectDB(body.user, body.password);
-    
-    const result = await client.query(body.query, body.variables);
-    
-    client.end();
-
-    return res.json(result);
-  } catch (error: any) {
-    console.error(error.message);
-
-    return res.status(500).json({ error: error.message });
-  }
-});
-
-const server = app.listen(PORT, () =>
-  console.log(`Server started on port ${PORT}`)
-);
+server.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 
 export default server;
