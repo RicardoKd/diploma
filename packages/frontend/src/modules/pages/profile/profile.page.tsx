754dc0be372f1557e618ec2e6199b2278d94bc62
RicardoKd <richardo.kadyan@gmail.com>
2024-02-06 14:48:04 +0200

    fix: profile page navigation

--- packages/frontend/src/modules/pages/profile/profile.page.tsx
+++ packages/frontend/src/modules/pages/profile/profile.page.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Typography } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
 
 import { ROUTER_KEYS } from '../../constants';
 import { getRole, logOut } from '../../utils';
 import { AppButton, MainStyled, RouterLink } from '../../UI';
 import { AddChildForm, AddParentForm } from '../../components';
 import { ProfileStyled, ButtonContainer } from './profile.styled';
-import { useNavigate } from 'react-router-dom';
 
 export const ProfilePage = () => {
   const role = getRole();
