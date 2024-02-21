c5102aa305538398593957fc4da9e2b4aceb2db7
RicardoKd <richardo.kadyan@gmail.com>
2024-02-21 15:54:00 +0200

    refactored: colors

--- packages/frontend/src/modules/theme/colors.const.ts
+++ packages/frontend/src/modules/theme/colors.const.ts
@@ -1,15 +1,18 @@
 export const COLORS = Object.freeze({
-  purple: '#6b5b95',
-  lightPurple: '#a886ff',
-  darkPurple: '#534E61',
+  darkPurple: '#6b5b95',
+  purple: '#a886ff',
+  lightPurple: '#bbb2e9',
+
+  greyishwhite: '#E9E9EA',
+  
+  blue: '#1976d2',
+
   grey: '#7f8fa6',
   white: '#ffffff',
   black: '#020202',
-  primary: '#bbb2e9',
   secondary: '#eeeeee',
   selected: '#add8e6',
   lightgrey: '#d3d3d3',
   success: '#65C466',
-  greyishwhite: '#E9E9EA',
   red: '#f95976',
 });
