9387d0e496db50b6c786a2b02d7f1dd85e192fa4
RicardoKd <richardo.kadyan@gmail.com>
2024-02-23 18:06:40 +0200

    removed: redundant colors

--- packages/frontend/src/modules/theme/colors.const.ts
+++ packages/frontend/src/modules/theme/colors.const.ts
@@ -1,10 +1,8 @@
 export const COLORS = Object.freeze({
   blue: '#1976d2',
-  grey: '#5b5b5b',
   white: '#ffffff',
   black: '#000000',
   yellow: '#D18C19',
-  darkPurple: '#6b5b95',
   purple: '#a886ff',
   lightPurple: '#bbb2e9',
   greyishwhite: '#E9E9EA',
