61def165aea3e304908a1bb30d91801b8c478414
RicardoKd <richardo.kadyan@gmail.com>
2024-02-24 20:23:45 +0200

    feat: error handling onUpdate

--- packages/frontend/src/modules/components/TransactionsTable/TransactionsTable.component.tsx
+++ packages/frontend/src/modules/components/TransactionsTable/TransactionsTable.component.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import { AxiosError } from 'axios';
+import DeleteIcon from '@mui/icons-material/Delete';
 import { useMutation, useQuery } from 'react-query';
+import { GridActionsCellItem, GridColDef } from '@mui/x-data-grid';
 
 import { Table } from '../../UI';
-import { useAppSnackbar } from '../../utils';
+import { FormItems } from './FormItems';
+import { QUERY_KEYS } from '../../constants';
 import queryClient from '../../app/queryClient';
 import { transactionService } from '../../services';
 import { ICategory, ITransaction } from '../../types';
-import {
-  QUERY_KEYS,
-  GET_TRANSACTIONS_TABLE_COLUMN_DEFINITIONS,
-} from '../../constants';
+import { validationSchema } from './validationSchema';
+import { currencyFormatter, useAppSnackbar } from '../../utils';
 
 export const TransactionsTable = () => {
   const { showError, showSuccess } = useAppSnackbar();
@@ -81,17 +82,101 @@ export const TransactionsTable = () => {
     []
   );
 
+  const validateCellUpdate = async (cellName: string, cellValue: any) => {
+    let isError = false;
+    console.log({ [cellName]: cellValue });
+    await validationSchema
+      .validateAt(cellName, { [cellName]: cellValue })
+      .catch((error) => {
+        showError(error.message);
+        isError = true;
+      });
+
+    return isError;
+  };
+
+  const columns: GridColDef[] = [
+    {
+      flex: 0.35,
+      field: 'type',
+      headerName: 'Type',
+    },
+    {
+      flex: 0.5,
+      type: 'number',
+      editable: true,
+      field: 'amount_of_money',
+      headerName: 'Amount of money',
+      valueFormatter: ({ value }) => currencyFormatter.format(value),
+      preProcessEditCellProps: async ({ props, row }) => {
+        if (row.type === 'income') {
+          props.error = await validateCellUpdate(
+            FormItems.amountOfMoney,
+            props.value
+          );
+        }
+
+        return props;
+      },
+    },
+    {
+      flex: 0.5,
+      type: 'date',
+      editable: true,
+      headerName: 'Date',
+      field: 'record_date',
+      preProcessEditCellProps: async ({ props }) => {
+        props.error = await validateCellUpdate(
+          FormItems.recordDate,
+          props.value
+        );
+
+        return props;
+      },
+    },
+    {
+      flex: 0.5,
+      editable: true,
+      field: 'category',
+      type: 'singleSelect',
+      headerName: 'Category',
+      valueGetter: ({ row }) => row.category.id,
+      valueSetter: ({ row, value }) => {
+        row.category.id = value;
+
+        return row;
+      },
+      valueOptions: ({ row }) =>
+        row && row.type === 'income'
+          ? incomeCategories || []
+          : spendCategories || [],
+    },
+    {
+      flex: 1.5,
+      field: 'notes',
+      editable: true,
+      headerName: 'Notes',
+    },
+    {
+      type: 'actions',
+      field: 'actions',
+      getActions: (params) => [
+        <GridActionsCellItem
+          label="Delete"
+          icon={<DeleteIcon />}
+          onClick={() => handleDelete(params.row)}
+        />,
+      ],
+    },
+  ];
+
   return (
     <Table
+      columns={columns}
       isLoading={isLoading}
       sx={{ minWidth: 1000 }}
       handleUpdate={handleUpdate}
       rows={transactionsLoaded ? transactions : []}
-      columns={GET_TRANSACTIONS_TABLE_COLUMN_DEFINITIONS({
-        handleDelete,
-        spendCategories,
-        incomeCategories,
-      })}
     />
   );
 };
