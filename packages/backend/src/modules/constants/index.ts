3831e722dc6049c898c806b2d49a9a9f8de187cf
RicardoKd <richardo.kadyan@gmail.com>
2024-02-24 17:51:24 +0200

    added interval

--- packages/backend/src/modules/constants/index.ts
+++ packages/backend/src/modules/constants/index.ts
@@ -2,7 +2,8 @@ export const PORT = 4200;
 
 export const ROLES = ['super_parent', 'parent', 'child'];
 
-export const CRON_INTERVAL = '*/1 * * * *';
+// export const CRON_INTERVAL = '*/1 * * * *'; // every min
+export const CRON_INTERVAL = '0 0 * * *'; // every 00:00
 
 export const PG_CONF = Object.freeze({
   port: 5432,
